b670ced899956318d0c33e641d15bd0c3adea05a
Svetlana Omelkova <svetlana@greenhouseci.com>
2017-04-19 10:05:29 +0300

    fix typos

--- test.py
+++ test.py
@@ -6,8 +6,8 @@ class TestBankAccount(unittest.TestCase):
     def setUp(self):
         pass
     
-    def some_test(self):
-        self.assertTrue()
+    def test_something(self):
+        self.assertTrue(True)
 
     # Your code goes here
 
