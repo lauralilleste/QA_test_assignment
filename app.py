77d34b1b2bb8bb16c67e3453f2dac31ab24bc94c
Svetlana Omelkova <svetlana.omelkova@gmail.com>
2017-04-25 21:13:17 +0300

    replace third party web service

--- app.py
+++ app.py
@@ -46,27 +46,27 @@ class BankAccount(object):
 
 
 class Transaction(object):
-    IP_SERVICE_REQUEST_URL = "http://ip.jsontest.com/"
+    IP_SERVICE_REQUEST_URL = "http://jsonplaceholder.typicode.com/posts/1"
     
     def __init__(self, initial_amount, final_amount):
-        self.ip_address = self._get_ip()
+        self.user_id = self._get_id()
         self.timestamp = datetime.datetime.now()
         self.initial_amount = initial_amount
         self.final_amount = final_amount
         self.difference = final_amount - initial_amount
         
-    def _get_ip(self):
+    def _get_id(self):
         """
         Make a http request to third party web service in order to get current ip address
         """
         resp = requests.get(self.IP_SERVICE_REQUEST_URL)
         if not resp.status_code == 200:
             raise Exception("Can not obtain IP address")
-        return json.loads(resp.content)["ip"]
+        return json.loads(resp.content)["userId"]
     
     def __str__(self):
         return "    ".join(map(str,[self.timestamp, 
-                                  self.ip_address, 
+                                  self.user_id, 
                                   self.initial_amount, 
                                   self.final_amount,
                                   self.difference]))
@@ -97,7 +97,7 @@ def withdraw_from_bank_account(account):
 
 def get_transaction_history(account):
     if account.get_transaction_history():
-        print("ID   Timestamp    IP address  initial amount  final amount    difference")
+        print("ID   Timestamp    user Id  initial amount  final amount    difference")
         for i, tr in enumerate(account.get_transaction_history()):
             print(str(i+1)+"   "+str(tr))
     else:
